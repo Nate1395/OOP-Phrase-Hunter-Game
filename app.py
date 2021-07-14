f322ad690a7d2ebe062be5948e8b8efc1fc75ef0
Nathan Zelaya <enzelaya1395@gmail.com>
2021-07-14 17:36:29 -0500

    Update app.py
    Accidentally left the instance creation and method call outside of the dunder main. Had 'pass' inserted inside the dunder main because of test code.

--- app.py
+++ app.py
@@ -2,12 +2,10 @@
 from game import Game
 
 
-game = Game()
-game.start()
-
 # Create your Dunder Main statement.
 if __name__ == "__main__":
-    pass
+    game = Game()
+    game.start()
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
